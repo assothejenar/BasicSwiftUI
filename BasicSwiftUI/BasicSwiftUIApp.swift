618ce4ab9fec76b56cc92d04dcb45782be59b905
Gennaro Caccavallo <caccavallogennaro@gmail.com>
2022-10-19 17:27:52 +0200

    Create project

---
+++ BasicSwiftUI/BasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIApp.swift
+//  BasicSwiftUI
+//
+//  Created by Gennaro Caccavallo on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicSwiftUI/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  BasicSwiftUI
+//
+//  Created by Gennaro Caccavallo on 18/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .padding(.vertical)
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
